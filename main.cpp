0b13545c0487b1379fa2bd3702f15268443e9845
carsoncrum <carsoncrum07@gmail.com>
2024-09-23 16:26:01 +0000

    added code

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@ int main()
 
 {
 
+  const int tank = 20;
+ const double town = 23.5;
+  const double highway = 28.9;
+  
+
+  double town_distance = town * tank;
+  double highway_distance = highway * tank;
+
+  cout << "A car with a 20-gallon gas tank averages 23.5 miles per gallon when driven in town, and 28.9 miles per gallon when driven on the highway.\n\n";
+  cout << "Below shows the distances the car's tank can travel in the town and on the highway:\n\n";
+  
+  cout << "Distance in town: " << town_distance << " miles" << endl;
+  cout << "Distance on highway: " << highway_distance << " miles" << endl;
+
 
   return 0;
 }
