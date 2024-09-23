c68304294a93803d400b6e35225a142d66bb22af
carsoncrum <carsoncrum07@gmail.com>
2024-09-23 16:36:15 +0000

    Added code

--- .ccls-cache/@home@runner@Chapter-2-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Programming-Project/main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main()
 
 {
+const int surveyed_customers = 16500;
+const double enery_percentage = 0.15;
+const double citrus_percentage = 0.58;
 
+int energy_drink_customers = surveyed_customers * enery_percentage;
+int citrus_drink_customers = energy_drink_customers * citrus_percentage;
+
+ cout << "The approximate number of energy drink customers in the survey is " << energy_drink_customers << endl;
+ cout << "The approximate number of citrus flavored energy drink customers in the survey is " << citrus_drink_customers << endl;
 
   return 0;
 }
