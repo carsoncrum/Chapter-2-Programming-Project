ac3274fbd3d1cba5233e7c3e1ad0b8e5afb4bb21
carsoncrum <carsoncrum07@gmail.com>
2024-09-23 16:06:53 +0000

    added code for ocean levels

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main()
 
 {
-
-
+const double mm_per_year = 1.5;
+  
+  cout << "In 5 years the ocean level will be higher by " << mm_per_year * 5 << " mm" << endl;
+  cout << "In 7 years the ocean level will be higher by " << mm_per_year * 7 << " mm" << endl;
+  cout << "In 10 years the ocean level will be higher by " << mm_per_year * 10 << " mm" << endl;
+  cout << endl;
+  
   return 0;
 }
